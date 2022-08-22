c7bb2b7dd5f63aac9343da757d6c1ab86cb609ea
techAJAY <ajay2681998@gmail.com>
2022-08-22 17:17:20 +0530

    mysql all query

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const DataSource = require('loopback-datasource-juggler').DataSource;
+
+const config = require('rc')('loopback', {dev: {mysql: {}}}).dev.mysql;
+
+const ds = new DataSource(require('../'), config);
+
+function show(err, models) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(models);
+    if (models) {
+      models.forEach(function(m) {
+        console.dir(m);
+      });
+    }
+  }
+}
+
+ds.discoverModelDefinitions({views: true, limit: 20}, show);
+
+ds.discoverModelProperties('customer', show);
+
+ds.discoverModelProperties('location', {owner: 'strongloop'}, show);
+
+ds.discoverPrimaryKeys('customer', show);
+ds.discoverForeignKeys('inventory', show);
+
+ds.discoverExportedForeignKeys('location', show);
+
+ds.discoverAndBuildModels('weapon', {owner: 'strongloop', visited: {}, associations: true}, function(err, models) {
+  for (const m in models) {
+    models[m].all(show);
+  }
+});
